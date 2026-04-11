55f1a9fc6ed47f2d9a8f557dfa6ffa67f5b9cca9
judyadabie <judyadabie@gmail.com>
2026-04-11 14:35:52 -0400

    event updates

--- FGFB/Controllers/EventsController.cs
+++ FGFB/Controllers/EventsController.cs
@@ -7,7 +7,18 @@ namespace FGFB.Controllers
         [HttpGet]
         public IActionResult ChampionshipDraftWeekend()
         {
-            return View();
+            return View("~/Views/Events/ChampionshipDraftWeekend.cshtml");
+        }
+        [HttpGet]
+        public IActionResult ChampionshipDraftWeekend2()
+        {
+            return View("~/Views/Events/ChampionshipDraftWeekend2.cshtml");
+        }
+
+        [HttpGet]
+        public IActionResult ChampionshipDraftWeekendSignup()
+        {
+            return View("~/Views/Events/ChampionshipDraftWeekendSignup.cshtml");
         }
     }
 }
